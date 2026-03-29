d27e6fce99f4bdbe4f98fd82417897569c78c14c
VidArg86 <tomasfarao03@gmail.com>
2026-03-29 01:55:28 -0300

    Update warning message in Quantumization.py

--- Quantumization.py
+++ Quantumization.py
@@ -4,4 +4,4 @@
 # NOTICE: This resource was purged to maintain local causality.
 # Reason: "[Redacted] [Redacted] give [Redacted] up, [Redacted] [Redacted] [Redacted] [Redacted] d now."
 #
-# WARNING: Further attempts to 'git pull' this reality will result in a 410 of [Redacted].
+# WARNING: Further attempts to 'git pull' this reality will result in a 410 of the observer.
